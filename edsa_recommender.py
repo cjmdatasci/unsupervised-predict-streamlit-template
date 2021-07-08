cd795098f49a687a20507b6821ce95fede8d6ae3
cjmdatasci <cjm.datasci@gmail.com>
2021-07-08 16:56:47 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -45,7 +45,7 @@ def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview, "Company Information, Background & Team"]
+    page_options = ["Recommender System","Solution Overview", "Company Information, Background & Team"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
@@ -108,56 +108,53 @@ def main():
 ################################ EDA ######################################################
 ###########################################################################################
 	# Building out the "Company Information, Background & Team" page
+    if page_selection == "Company Information, Background & Team":
+                st.info("Meet the amazing team members that contributed to this project.")
+                # st.markdown("<h1 style='text-align: center;'>Contributors</h1>", unsafe_allow_html=True)
+                # st.markdown("\n\n")
 
-if selection == "Company Information, Background & Team":
-		st.title("Company Information, Background and Team")
-		st.info('Discover the mission and vision that keeps us going as well as the amazing team that pulled this project together and how we started.')
+                ### IN ALPHABETICAL ORDER ###
+                # Bulelani
+                st.markdown("<h3 style='text-align: center;'>Bulelani Nkosi</h3>", unsafe_allow_html=True)
+                st.markdown("<p style='text-align: center;'>Streamlit App Coordinator | Data Analyst</p>", unsafe_allow_html=True)
+                st.image('resources/imgs/Bulelani.jpg', width=120)
+                st.markdown("<a href='https://www.linkedin.com/in/bulelanin' target='_blank'>LinkedIn</a>", unsafe_allow_html=True)
+                st.markdown("<a href='https://github.com/BNkosi' target='_blank'>GitHub</a>", unsafe_allow_html=True)
 
-		st.header('Our Mission')		
-		st.write('To use AI to combat climate change within Africa, securing the futures of the generations of now and tomorrow.')
+                # Lizette
+                st.markdown("<h3 style='text-align: center;'>Lizette Loubser</h3>", unsafe_allow_html=True)
+                st.markdown("<p style='text-align: center;'>Notebook Coordinator | ML Engineer</p>", unsafe_allow_html=True)
+                st.image('resources/imgs/Lizette.jpg', width=120)
+                st.markdown("<a href='http://www.linkedin.com/in/lizette-loubser' target='_blank'>LinkedIn</a>", unsafe_allow_html=True)
+                st.markdown("<a href='https://github.com/Lizette95' target='_blank'>GitHub</a>", unsafe_allow_html=True)
 
-		st.header('Our Vision')
-		st.write('A better and more intelligent Africa which is able to adapt to the fourth industrial revolution by using Data Science, for social good.')
+                # Neli
+                st.markdown("<h3 style='text-align: center;'>Nelisiwe Mabanga</h3>", unsafe_allow_html=True)
+                st.markdown("<p style='text-align: center;'>Data Journalist | Analyst</p>", unsafe_allow_html=True)
+                st.image('resources/imgs/nelly.jpeg', width=120)
+                st.markdown("<a href='https://www.linkedin.com/in/nelisiwe-mabanga-8bb409106/' target='_blank'>LinkedIn</a>", unsafe_allow_html=True)
+                st.markdown("<a href='https://github.com/Phiwe-Mabanga' target='_blank'>GitHub</a>", unsafe_allow_html=True)
 
-		st.header('Our Amazing Team')
-		st.write('A team of 6 passionate AI solutionists.')
-		#First row of pictures
+                # Nolu
+                st.markdown("<h3 style='text-align: center;'>Noluthando Khumalo</h3>", unsafe_allow_html=True)
+                st.markdown("<p style='text-align: center;'>Streamlit App Designer</p>", unsafe_allow_html=True)
+                st.image('resources/imgs/Thando.jpg', width=120)
+                st.markdown("<a href='https://www.linkedin.com/in/noluthando-khumalo-3870ab191/' target='_blank'>LinkedIn</a>", unsafe_allow_html=True)
+                st.markdown("<a href='https://github.com/ThandoKhumalo' target='_blank'>GitHub</a>", unsafe_allow_html=True)
 
-		col1, col2,col3 = st.beta_columns(3)
-		Ric_Pic =Image.open('resources/imgs/Rickie_pic.png') 
-		col1.image(Ric_Pic,caption="Rickie Mogale Mohale", width=150)
-		col1.write('Tech-lead and software developer.')
-        
-		Cot_Pic =Image.open('resources/imgs/courtney_pic.png') 
-		col2.image(Cot_Pic,caption="Courtney Murugan", width=150)
-		col2.write('Machine learning engineer')
+                # Nompilo
+                st.markdown("<h3 style='text-align: center;'>Nompilo Nhlabathi</h3>", unsafe_allow_html=True)
+                st.markdown("<p style='text-align: center;'>Streamlit App Designer</p>", unsafe_allow_html=True)
+                st.image('resources/imgs/Nompilo.png', width=120)
+                st.markdown("<a href='http://www.linkedin.com/in/nompilo-nhlabathi-2701791b2' target='_blank'>LinkedIn</a>", unsafe_allow_html=True)
+                st.markdown("<a href='https://github.com/mapilos' target='_blank'>GitHub</a>", unsafe_allow_html=True)
 
-		Cot_Pic =Image.open('resources/imgs/jacques_pic.png') 
-		col3.image(Cot_Pic,caption="Jacques Stander", width=150)
-		col3.write('Project manager')
-
-        #Second row of pictures
-		col4, col5,col6 = st.beta_columns(3)
-		vesh_Pic =Image.open('resources/imgs/veshen_pic.png') 
-		col4.image(vesh_Pic,caption="Veshen Naidoo", width=150)
-		col4.write('UX/UI Designer')
-        
-		Phiw_Pic =Image.open('resources/imgs/phiwe_pic.png') 
-		col5.image(Phiw_Pic,caption="Phiweka Mthini", width=150)
-		col5.write('Digital marketer ')
-
-		nor_Pic =Image.open('resources/imgs/nour_pic.png') 
-		col6.image(nor_Pic,caption="Nourhan Alfalous", width=150)
-		col6.write('Database architect')
-
-		#Third row of picture 
-		col7, col8,col9 = st.beta_columns(3)
-
-		st.header('How we started?')
-		st.write('African Intelligence started as a group of 6 students who met each other on a university project. The students bonded together around a love for solving problems with the help of AI. ')	
-		st.write('These students all graduated with flying colours and entered successful carreers, but they never forgot the joys of solving real world problems.')
-		st.write('A few years later they decided to meet up again and started working part time on this project which they call: AI Africa.')
-
+                # Sizwe
+                st.markdown("<h3 style='text-align: center;'>Sizwe Bhembe</h3>", unsafe_allow_html=True)
+                st.markdown("<p style='text-align: center;'>Notebook Assistant</p>", unsafe_allow_html=True)
+                st.image('resources/imgs/Sizwe.jpg', width=120)
+                st.markdown("<a href='https://www.linkedin.com/in/sizwe-bhembe-372880101' target='_blank'>LinkedIn</a>", unsafe_allow_html=True)
+                st.markdown("<a href='https://github.com/sjbhembe' target='_blank'>GitHub</a>", unsafe_allow_html=True)
 
 #
 
