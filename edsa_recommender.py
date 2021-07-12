0996c04c832487ea43c6cd57ff868cd5f0530c39
cjmdatasci <cjm.datasci@gmail.com>
2021-07-12 11:40:36 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -61,7 +61,8 @@ def main():
         contents = file_.read()
         data_url = base64.b64encode(contents).decode("utf-8")
         file_.close()
-        st.markdown(f'<img src="data:image/gif;base64,{data_url},text-align: center;" alt="cat gif">',unsafe_allow_html=True,)
+        st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',unsafe_allow_html=True,)	
+
     
         page_options = ["Recommender System","Solution Overview", "Company Information, Background & Team"]
 
