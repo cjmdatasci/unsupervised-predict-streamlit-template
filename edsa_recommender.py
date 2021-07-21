491165645e6ebcaad9986d9d3965583cc4306772
cjmdatasci <cjm.datasci@gmail.com>
2021-07-21 15:45:39 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -192,18 +192,18 @@ def main():
 
     if page_selection == "Exploratory Data Analysis":
             #title_tag("Insights extracted from the data")
-            visual_options = ["The top 15 movies", "Genres with the most number movies", "A count of films by directors"]
-            visual_selection = st.selectbox("Choose Exploratory Data Analaysis Visuas Option", visual_options)
+            visual_options = ["The top 10 actors", "Genres with the most number movies", "A count of films by directors"]
+            visual_selection = st.selectbox("Select Option", visual_options)
 
-            if visual_selection == "The top 15 movies":
+            if visual_selection == "The top 10 actors":
                 subheading('Top 15 movies by number of Ratings')
-                st.image('resources/imgs/top_15_titles.png',use_column_width=True)
+                st.image('resources/imgs/act.png',use_column_width=True)
             elif visual_selection == "Genres with the most number movies":
                 subheading('Genres with the most number movies')
                 st.image('resources/imgs/Genres.png',use_column_width=True)
             elif visual_selection == "A count of films by directors":
                 subheading('A count of films by directors')
-                st.image('resources/imgs/director.png',use_column_width=True)
+                st.image('resources/imgs/directors.png',use_column_width=True)
 
 #
 if __name__ == '__main__':
